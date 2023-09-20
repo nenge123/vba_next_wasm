9a7f365ba21efaf7b30d41a50bca20d3f512f8d5
nenge123 <m@nenge.net>
2023-09-20 15:35:04 +0800

    优化pwa离线缓存流程 确保正常通信

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ var CACHE_LIST = [
     "assets/images/zan.jpg"
 ].map(v => CACHE_PATH + v);
 var isLocal = location.host == '127.0.0.1';
-var version = '2023/09/20 12:16';
+var version = '2023/09/20 15:34';
 var myIDB;
 var PromiseList = {};
 var NowClient;
